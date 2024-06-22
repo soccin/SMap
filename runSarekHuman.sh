44c2de137b0a1b893c87157c9783cc6a8ffeca7b
Nicholas D. Socci <soccin@mskcc.org>
2024-06-21 21:26:13 -0400

    Create config folder

--- runSarekHuman.sh
+++ runSarekHuman.sh
@@ -2,17 +2,28 @@
 
 #curl -s https://get.nextflow.io | bash
 
-export NXF_SINGULARITY_CACHEDIR=/rtsess01/compute/juno/bic/ROOT/opt/singularity/cachedir_socci
-export TMPDIR=/fscratch/socci
-    
-#--tools freebayes \
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-./nextflow run sarek/main.nf -ansi-log false \
+export NXF_SINGULARITY_CACHEDIR=/rtsess01/compute/juno/bic/ROOT/opt/singularity/cachedir_socci
+export TMPDIR=/scratch/socci
+export PATH=$SDIR/bin:$PATH
+
+haveNextflow=$(which nextflow 2>/dev/null)
+
+if [ "$haveNextflow" == "" ]; then
+    echo -e "\n\n   Need to install nextflow; see docs\n\n"
+    exit 1
+fi
+
+set -ue
+
+    
+nextflow run sarek/main.nf -ansi-log false \
     -profile singularity \
-    -c neo.config \
+    -c $SDIR/config/neo.config \
     --genome GATK.GRCh37 \
     --outdir out \
     -resume \
-    --input input_sarek_somatic.csv
+    --input $INPUT
 
 
