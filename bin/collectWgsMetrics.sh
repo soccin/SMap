640dca972a95b97d9e5e0cd04a8254a5fa94c0a3
Nicholas D. Socci <soccin@mskcc.org>
2024-11-07 13:27:55 -0500

    Fix coverage limit in wgs metrics

--- bin/collectWgsMetrics.sh
+++ bin/collectWgsMetrics.sh
@@ -4,7 +4,7 @@ SDIR=$(dirname "$(readlink -f "$0")")
 
 if [ "$#" != "1" ]; then
     echo -e "\n   usage: collectWgsMetrics FILE.bam\n"
-    echo -e '      #BSUB: -n 8 -R "rusage[mem=4]" \n\n'
+    echo -e '      #BSUB: -n 8 -R "rusage[mem=4]" -W 24:00 \n\n'
     exit
 fi
 
@@ -38,4 +38,17 @@ case $GENOME in
 
 esac
 
-picardV2 CollectWgsMetrics READ_LENGTH=160 USE_FAST_ALGORITHM=true R=$GENOME_FILE I=$BAM O=$ODIR/$(basename ${BAM/.bam/.wgs.txt})
+module load samtools
+AVG_READ_LEN=$(
+    samtools view $BAM \
+        | head -10000 \
+        | cut -f10 \
+        | perl -ne 'print length($_),"\n"' \
+        | awk '{s+=$1/10000}END{print s}'
+    )
+
+picardV2 CollectWgsMetrics \
+    READ_LENGTH=$AVG_READ_LEN \
+    USE_FAST_ALGORITHM=true \
+    COVERAGE_CAP=1000 \
+    R=$GENOME_FILE I=$BAM O=$ODIR/$(basename ${BAM/.bam/.wgs.txt})
