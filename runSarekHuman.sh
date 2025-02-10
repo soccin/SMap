44584b41339a514ef179b0c2903781ffd412540b
Nicholas D. Socci <soccin@mskcc.org>
2025-02-10 14:41:09 -0500

    Add genome selection option to runSarekHuman.sh script
    - Implement command-line argument parsing for genome selection
    - Allow choosing between GATK.GRCh37 and GATK.GRCh38 genomes
    - Update usage instructions and error handling
    - Default to GATK.GRCh37 if no genome is specified

--- runSarekHuman.sh
+++ runSarekHuman.sh
@@ -20,11 +20,53 @@ if [ ! -e "$SDIR/sarek/main.nf" ]; then
     exit 1
 fi
 
+#
+# Process command line arguments
+# - -g|--genome: GATK.GRCh37 or GATK.GRCh38
+#
+
+GENOME="GATK.GRCh37"  # Default genome
+
+# Parse command line arguments
+ARGS=$(getopt -o 'g:' --long 'genome:' -n 'runSarekHuman.sh' -- "$@")
+
+if [ $? -ne 0 ]; then
+    echo "Failed to parse arguments" >&2
+    exit 1
+fi
+
+eval set -- "$ARGS"
+unset ARGS
+
+while true; do
+    case "$1" in
+        '-g'|'--genome')
+            GENOME="$2"
+            shift 2
+            continue
+            ;;
+        '--')
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error!" >&2
+            exit 1
+            ;;
+    esac
+done
+
 if [ "$#" -ne "1" ]; then
     echo
-    echo usage: runSarekHuman.sh input_sarek.csv
+    echo "usage: runSarekHuman.sh [-g|--genome GATK.GRCh37|GATK.GRCh38] input_sarek.csv"
     echo
     exit
+fi
+
+if [ "$GENOME" != "GATK.GRCh37" ] && [ "$GENOME" != "GATK.GRCh38" ]; then
+    echo -e "\n\n   Invalid genome: $GENOME"
+    echo -e "   Valid options are: GATK.GRCh37 or GATK.GRCh38\n\n"
+    exit 1
 fi
 
 INPUT=$(realpath $1)
@@ -32,7 +74,7 @@ INPUT=$(realpath $1)
 nextflow run $SDIR/sarek/main.nf -ansi-log false \
     -profile singularity \
     -c $SDIR/config/neo.config \
-    --genome GATK.GRCh37 \
+    --genome $GENOME \
     --outdir sbam \
     -resume \
     --input $INPUT
