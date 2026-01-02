5d7a00b28e7c4f9107cb75aae29f7b91f8c3d096
Nicholas D. Socci <soccin@mskcc.org>
2026-01-01 22:25:07 -0500

    fix(juno): update NXF_SINGULARITY_CACHEDIR path
    Changed JUNO cluster singularity cache directory from
    /rtsess01/compute/juno/bic/ROOT/opt/singularity/cachedir_socci
    to /juno/bic/work/socci/opt/singularity/cachedir_socci
    
    Required due to filesystem changes by sysadmins.

--- runSarekHuman.sh
+++ runSarekHuman.sh
@@ -43,7 +43,7 @@ if [ "$CLUSTER" == "IRIS" ]; then
 
 elif [ "$CLUSTER" == "JUNO" ]; then
 
-  export NXF_SINGULARITY_CACHEDIR=/rtsess01/compute/juno/bic/ROOT/opt/singularity/cachedir_socci
+  export NXF_SINGULARITY_CACHEDIR=/juno/bic/work/socci/opt/singularity/cachedir_socci
   export TMPDIR=/scratch/socci
   export WORKDIR=work/$UUID
   NF_LOCAL_CONFIG=neo.config
